55849ccc5d7245e0c843ad7976d3c4b9b2d7b058
Jacob <46667568+jacobvdl@users.noreply.github.com>
2024-11-14 11:34:50 +1030

    Fixed a bug that made the AI stay 1 cell away from the player

--- AIE_Starter/FollowBehaviour.cpp
+++ AIE_Starter/FollowBehaviour.cpp
@@ -5,11 +5,11 @@ void FollowBehaviour::Update(Agent* agent, float dt)
 	Agent* target = agent->GetTarget();
 
 	float dist = glm::distance(target->GetPosition(), lastTargetPosition);
-	if (dist > agent->GetNodeMap()->GetCellSize()) {
+	
+	if (dist >= agent->GetNodeMap()->GetCellSize()) {
 		lastTargetPosition = target->GetPosition();
 		agent->GoTo(lastTargetPosition);
-	}
-}
+	}}
 
 void FollowBehaviour::Enter(Agent* agent)
 {
